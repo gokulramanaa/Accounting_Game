49917704fe872e719d312a980ab5acf44e7ee7c0
Gokulramanan Soundararajan <31602253+gokulramanaa@users.noreply.github.com>
2018-03-27 15:19:53 -0400

    Add files via upload

---
+++ imageFunt.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 24 22:37:17 2018
+
+@author: gokul
+"""
+
+from tkinter import *
+import Labelclass as Lc
+
+def ImgFunct(master):
+    photo = PhotoImage(file="C:/Users/gokul/Documents/Projects/AccountingGame/imgs/inst.png")
+    w = Label(master, image=photo)
+    w.photo = photo
+    w.pack()
+    
+Frame1 = Tk()
+TopFrame = Frame(Frame1, width=900, height=600)
+TopFrame.pack()
+TopFrame.pack_propagate(0)
+ImgFunct(TopFrame)
+Lc.Buttonclass(Frame1,"Start")
+Frame1.mainloop()
